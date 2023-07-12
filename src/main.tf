53d1f5673f534424ea8ea6d783b5381b77b4f68d
José Carlos Montañez <josecarlos.aragon@47deg.com>
2023-07-12 18:26:48 +0200

    added root block device

--- src/main.tf
+++ src/main.tf
@@ -10,11 +10,11 @@ provider "aws" {
 resource "aws_instance" "base" {
   ami           = "ami-0a8b4cd432b1c3063"
   instance_type = "t2.micro"
+  root_block_device {
+    encrypted = true
+  }
 }
 
 resource "aws_eip" "base" {
   instance = aws_instance.base.id
-  root_block_device {
-    encrypted = true
-  }
 }
